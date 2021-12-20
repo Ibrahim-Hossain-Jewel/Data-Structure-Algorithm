df0aee89c242c76c9abcd6b7e7c03ee7e441c8f1
Ibrahim Hossain <ibrahimhossaincse@gmail.com>
2021-12-20 10:56:00 +0600

    property core refactored

--- propertyGeneration.js
+++ propertyGeneration.js
@@ -1,16 +1,29 @@
 //Make a program that generator a property and value of object;
 //Refactoring the object generator program.
 
+// const objGenerator = (str)=>{
+//     const object = {};
+//     for(var i = 0;i<str.length;i++){
+//         const char = str[i];
+//         if(/[A-Za-z0-9]/.test(char)){
+//             if(object[char] > 0){
+//                 object[char]++;
+//             }else{
+//                 object[char] = 1;
+//             }
+//         }
+//     }
+//     return object;
+// }
+// console.log(objGenerator("Jehello"));
+
+
 const objGenerator = (str)=>{
     const object = {};
     for(var i = 0;i<str.length;i++){
         const char = str[i];
         if(/[A-Za-z0-9]/.test(char)){
-            if(object[char] > 0){
-                object[char]++;
-            }else{
-                object[char] = 1;
-            }
+            object[char] = object[char] || 1;
         }
     }
     return object;
