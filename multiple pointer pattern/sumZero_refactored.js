445156fbed9cf58f211851536fe765806291c664
Ibrahim Hossain <ibrahimhossaincse@gmail.com>
2022-01-10 10:15:54 +0600

    sumZero refactored program

---
+++ multiple pointer pattern/sumZero_refactored.js
@@ -0,0 +1,1 @@
+//Writings a function called sumZero which accepts a sorted array of integers. the function should find the first pair where the sum is zero. return an array that includes both values the sum to 0 or undefined if a pair doesn't exist.
